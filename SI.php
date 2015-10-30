247350a56e16945ba5ed84651b462f238bba1cf9
mark <mpdroog@rootdev.nl>
2015-10-30 13:26:37 +0100

    Feature. Pretty units with prefix

---
+++ SI.php
@@ -0,0 +1,11 @@
+<?php
+namespace core;
+
+/** International System of Units */
+class SI {
+	public static function prefix($size, $base = 'B') {
+	    $units = array( '', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y');
+	    $power = $size > 0 ? floor(log($size, 1024)) : 0;
+	    return number_format($size / pow(1024, $power), 2, '.', ',') . ' ' . $units[$power] . $base;
+	}
+}
