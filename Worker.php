1a4c945808b3f9536515768266a7a5e13b2ab9b9
MP Droog <mpdroog@icloud.com>
2015-12-10 12:09:39 +0100

    Feature. Run one job (with timeout of 10sec) in testing-mode for validation tests

--- Worker.php
+++ Worker.php
@@ -4,9 +4,22 @@ use Pheanstalk\Pheanstalk;
 
 class Worker {
 	public static function listen($channel, $fn) {
+		global $_CLI;
+
 		$queue = new Pheanstalk("127.0.0.1");
 		$queue->watch($channel)->ignore("default");
 		msg("Process $channel(127.0.0.1)");
+
+		if ($_CLI["test"]) {
+			// Process 1 cmd in testing mode
+			$job = $queue->reserve(10);
+			msg(sprintf("Processing job (%d)", $job->getId()), [$job->getData()]);
+			$input = json_decode($job->getData(), true);
+
+			$fn($input);
+			$queue->delete($job);
+			return;
+		}
 		while (true) {
 			$job = $queue->reserve();
 			msg(sprintf("Processing job (%d)", $job->getId()), [$job->getData()]);
