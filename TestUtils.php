cba1f8bd48bff73bbb7cab93ffdeed50b03eaeea
MP Droog <mpdroog@icloud.com>
2016-07-20 19:50:16 +0200

    Feature. More handy functions for testing

--- TestUtils.php
+++ TestUtils.php
@@ -1,6 +1,8 @@
 <?php
 namespace core;
+use core\Cli;
 use core\Curl;
+use core\Helper;
 
 trait TestUtils {
 	// Get cURL instance
@@ -11,5 +13,44 @@ trait TestUtils {
 			return $res;
 		});
 		return $c;
+	}
+
+	private function exec_worker($name, array $args = []) {
+		$param = array_merge([ROOT . "workers/index.php", $name, "-i=1"], $args);
+		return Cli::exec("php", $param);
+	}
+
+	private function exec_cli($name, array $args = []) {
+		$args = array_merge([$name], $args);
+		return Cli::exec("./run.sh", $args);
+	}
+
+	private static function pending($queue) {
+		try {
+			$queue = Helper::prefix($queue);
+			return self::$queue->statsTube($queue)["current-jobs-ready"];
+		} catch (\Exception $e) {
+			return 0;
+		}
+	}
+
+	private static function verbose($task, $text, $iswrite = false) {
+		$a = [
+			"test" => true,
+			"task" => $task,
+			"args" => [],
+			"flags" => [],
+			"today" => "2015-09-01"
+		];
+		if ($iswrite) {
+			$a["flags"]["w"] = true;
+		}
+
+		ob_start();
+		{
+			var_dump($a);
+		}
+		$txt = ob_get_clean();
+		return sprintf("%s%s\n", $txt, $text);
 	}
 }
