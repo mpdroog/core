50996614416e3771a243f83c6097fd4b9c021aba
mark <mark@xsnews.nl>
2018-02-21 08:11:31 +0100

    Replace. AES-class with Safe-class

---
+++ Safe.php
@@ -0,0 +1,25 @@
+<?php
+namespace core;
+use Defuse\Crypto\Key;
+use Defuse\Crypto\Crypto;
+
+/**
+ * Safe
+ */
+class Safe {
+	/**
+	 * Encode a string.
+	 */
+	public static function encode($value, $privKey) {
+		$key = Key::loadFromAsciiSafeString($privKey);
+                return base64_encode(Crypto::encrypt($value, $key, true));
+	}
+
+	/**
+	 * Decode a string.
+	 */
+	public static function decode($value, $privKey) {
+		$key = Key::loadFromAsciiSafeString($privKey);
+		return Crypto::decrypt(base64_decode($value), $privKey, true);
+	}
+}
