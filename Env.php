d01f0cbefb759447b91e4f5bd21749df4525d7a9
root <root@aws001.xsnews.intern>
2018-10-04 16:00:26 +0200

    Feature. Env(ironment) class to read SESSION in sane-way

---
+++ Env.php
@@ -0,0 +1,38 @@
+<?php
+namespace core;
+
+/**
+ * Safe to read environment-variables.
+ */
+class Env {
+    private static $server = null;
+
+    public static function init() {
+        if (! isset($_SERVER)) {
+            user_error("DevErr: Env-class loaded without SERVER-var");
+        }
+        self::$server = $_SERVER;
+    }
+
+    // Visitor IP
+    public static function ip() {
+        return self::$server[""HTTP_X_REAL_IP""] || self::$server["REMOTE_ADDR"];
+    }
+    // HTTP-protocol spoken to client.
+    public static function protocol() {
+         return self::$server["SERVER_PROTOCOL"];
+    }
+    public static function encoding() {
+        $enc = "plain";
+        if (isset($_SERVER["HTTP_ACCEPT"])) {
+            if ($_SERVER["HTTP_ACCEPT"] === "application/json") {
+                 $enc = "json";
+            }
+            if (strpos($_SERVER["HTTP_ACCEPT"], "text/html")) {
+                 $enc = "html";
+            }
+        }
+        return $enc;
+    }
+}
+
