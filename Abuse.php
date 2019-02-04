f85fd60f190bff0326f8d908a86e2ecd296739ae
MP Droog <mpdroog@icloud.com>
2019-02-04 16:57:33 +0100

    Simplify. Abuse
    [BREAKING]

--- Abuse.php
+++ Abuse.php
@@ -1,51 +1,51 @@
 <?php
 namespace core;
 use prj\Fn;
+use core\Db;
 use core\Res;
 
+/** Force 24hour wait after last attempt (Useful for user specific limits) */
+const STRATEGY_24H_WAIT = "STRATEGY_24H_WAIT";
+/** Force 24hour wait from the first attempt (Useful for generic limits i.e. 100 lostpass a day) */
+const STRATEGY_24H_ADD = "STRATEGY_24H_ADD";
+
 /**
- * Abuse helpers.
- * Simple methods to ban users that try
- * to flood specific features of the system.
+ * Simple abuse counter by IP.
  */
 class Abuse {
-	/** Check if we blocked the IP. (on abuse return 403 and block user until EXPIRE removed by banclear-cron) */
-	public static function req($ip) {
-		$db = Fn::db();
-		$blacklisted = $db->getCell("SELECT 1 FROM `blacklist_ip` WHERE ip=? AND active=?", [$ip, "1"]);
-		if ($blacklisted === "1") {
-			error_log(sprintf("Block IP(%s) due to abusive calls.", $ip));
-                        Res::error(403);
-			echo "Blacklisted.";
-			exit;
-		}
-	}
+        public static function whitelisted($ip) {
+            return in_array($ip, []); // TODO: Fn::whitelist() ?
+        }
 
-	/** Increase the abuse counter */
-	public static function increase($ip, $key = "abuse", $comment = "", $attempts = 10) {
-		$db = Fn::db();
-		$count = $db->getCell("SELECT count FROM `blacklist_ip` WHERE `ip` = ? and `key` = ?", [$ip, $key]);
-		if ($count === false) {
-			$count = 1;
-			$db->insert("blacklist_ip", [
-				"key" => $key, "count" => $count,
-				"ip" => $ip, "expire" => strtotime("+2 days"),
-			]);
-		} else {
-			$count = intval($count);
-			$count++;
-			$db->update("blacklist_ip", [
-				"count" => $count,
-				"expire" => strtotime("+2 days"),
-				"active" => $count >= $attempts ? "1" : "0"
-			], [
-				"ip" => $ip,
-				"key" => $key
-			]);
-		}
-		error_log(sprintf(
-			"IP(%s) Abuse++(%s) Key(%s) comment=%s",
-			$ip, $count, $key, $comment
-		));
-	}
+        public static function incr($ip, $max=60, $strategy = STRATEGY_24H_WAIT) {
+            $now = time();
+            $db = Fn::Db();
+            if ($strategy === STRATEGY_24H_WAIT) {
+                $db->exec(
+                    "DELETE FROM abuselimit WHERE ratelimit_time_updated < ?",
+                    [strtotime("-1 day")]
+                );
+            } else if ($strategy === STRATEGY_24H_ADD) {
+                $db->exec(
+                    "DELETE FROM abuselimit WHERE ratelimit_time_added < ?",
+                    [strtotime("-1 day")]
+                );
+            } else {
+                user_error("DevErr: Invalid Abuse strategy=$strategy");
+            }
+
+            $whitelisted = self::whitelisted($ip);
+            $count = $db->getCell("SELECT ratelimit_count FROM abuselimit WHERE ratelimit_ip = ? LIMIT 1", [$ip]);
+            if (!$whitelisted && $count > $max) {
+                Res::error(503);
+                echo "Err, too many requests.";
+                exit;
+            }
+
+            $db->exec(
+                "INSERT INTO abuselimit (`ratelimit_ip`, `ratelimit_count`, `ratelimit_time_updated`, `ratelimit_time_added`) VALUES(?, ?, ?, ?)
+                ON DUPLICATE KEY UPDATE ratelimit_count=ratelimit_count+1,ratelimit_time_updated=?",
+                [$ip, 1, $now, $now, $now]
+            );
+        }
 }
