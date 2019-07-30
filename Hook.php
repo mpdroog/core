8724d89fa12e8caaed0324a06f78cc11ec59d857
MP Droog <mpdroog@icloud.com>
2019-07-30 11:37:30 +0200

    Create Hook.php
    Dead simple Hooking mechanism useful for loading some code when multiple parts of the codebase need the same thing done.

---
+++ Hook.php
@@ -0,0 +1,26 @@
+<?php
+namespace core;
+
+class Hook {
+    private static $hooks = [];
+
+    public static function add($name) {
+        if (isset(self::$hooks[$name])) {
+            die("DevErr: hook[$name] already added");
+        }
+        self::$hooks[$name] = [];
+    }
+
+    public static function listen($name, $file) {
+        if (isset(self::$hooks[$name][$file])) {
+            die("DevErr: hook[$name][$file] already added");
+        }
+        self::$hooks[$name][$file] = true;
+    }
+
+    public static function trigger($name, array $args) {
+        foreach (self::$hooks[$name] as $fn => $meta) {
+            require $fn;
+        }
+    }
+}
