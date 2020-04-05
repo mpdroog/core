fe3014190e3fef5049135cd27c2192ec8d2571ca
MP Droog <mpdroog@icloud.com>
2020-04-05 17:56:13 +0200

    Feature. Convert::csv
    Parse csvstring into an associated array

---
+++ Convert.php
@@ -0,0 +1,24 @@
+<?php
+namespace core;
+
+/**
+ * Parse content-type
+ */
+class Convert {
+  // Parse CSV into associated array
+  public static function csv($str) {
+    $fp = fopen("php://temp", 'r+');
+    fputs($fp, $str);
+    rewind($fp);
+
+    $lines = [];
+    $kv = fgetcsv($fp);
+    while ( ($data = fgetcsv($fp) ) !== FALSE ) {
+      $out = [];
+      foreach ($data as $idx => $col) {
+        $out[ trim($kv[$idx]) ] = trim($col);
+      }
+      $lines[] = $out;
+    }
+  }
+}
