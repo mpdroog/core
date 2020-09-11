b0d11951aaa6d10830d67d0191eac56c52e00017
MP Droog <mpdroog@icloud.com>
2020-09-11 09:46:38 +0200

    Add strict checking to Convert

--- Convert.php
+++ Convert.php
@@ -8,15 +8,32 @@ class Convert {
   // Parse CSV into associated array
   public static function csv($str) {
     $fp = fopen("php://temp", 'r+');
-    fputs($fp, $str);
-    rewind($fp);
+    if (! is_resource($fp)) {
+      user_error("fopen(temp) failed");
+    }
+    if (false === fputs($fp, $str)) {
+      user_error("fputs(temp) failed");
+    }
+    if (! rewind($fp)) {
+      user_error("rewind(temp) failed");
+    }
+
+    $kv = fgetcsv($fp);
+    if ($kv === null || $kv === false) {
+      user_error("fgetcsv(temp) failed");
+    }
+    foreach ($kv as $k => &$v) {
+      $v = trim($v);
+    }
 
     $lines = [];
-    $kv = fgetcsv($fp);
     while ( ($data = fgetcsv($fp) ) !== FALSE ) {
       $out = [];
       foreach ($data as $idx => $col) {
-        $out[ trim($kv[$idx]) ] = trim($col);
+        if (isset($out[ $kv[$idx] ])) {
+          user_error(sprintf("key=%s already set", $kv[$idx]));
+        }
+        $out[ $kv[$idx] ] = trim($col);
       }
       $lines[] = $out;
     }
