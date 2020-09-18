0100bcfaa1ceef768691cf8fc7dd9e45ccbb1437
MP Droog <mpdroog@icloud.com>
2020-09-18 11:19:22 +0200

    Files::delfiles
    Remove all files in a directory

--- Files.php
+++ Files.php
@@ -32,4 +32,24 @@ class Files {
     }
     return rmdir($dir);
   }
+
+  /** Delete directory content (recursively) */
+  public static function delfiles($dir) {
+    if (!file_exists($dir)) {
+        return true;
+    }
+    if (!is_dir($dir)) {
+        return unlink($dir);
+    }
+
+    foreach (scandir($dir) as $item) {
+        if ($item == '.' || $item == '..') {
+            continue;
+        }
+        if (!self::deldir($dir . DIRECTORY_SEPARATOR . $item)) {
+            return false;
+        }
+    }
+    return true;
+  }
 }
