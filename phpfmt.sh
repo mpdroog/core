9d6f2cb8f0d7a9d8a057653517319e63bef4bfb1
MP Droog <mpdroog@icloud.com>
2020-11-20 08:47:03 +0100

    Lib. phpfmt (php-cs-fixer) to auto-format code

---
+++ phpfmt.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+# Auto-format PHP
+set +e
+if [ ! -f "php-cs-fixer" ]; then
+        wget https://cs.symfony.com/download/php-cs-fixer-v2.phar -O php-cs-fixer
+        chmod a+x php-cs-fixer
+fi
+set -e
+
+php php-cs-fixer fix --config=.php_cs.dist .
