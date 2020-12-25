41ac3e1294e52b12fe54bd61f234bbcaf3d1c4b1
MP Droog <mpdroog@rootdev.nl>
2020-12-25 12:04:03 +0100

    Feature. Numeric::calc_decimals
    Calculate amount of decimals to show for wide-range numbers with different accuracy's needed.

--- Numeric.php
+++ Numeric.php
@@ -3,6 +3,39 @@ namespace core;
 
 class Numeric
 {
+        /**
+         * Calculate amount of decimals we want to show.
+         * You can forward this function result to the round/number_format funcs
+         *
+         * i.e. 5000.5555   = 5000.56
+         *      0.000055555 = 0.000056
+         */
+        public static function calc_decimals($val) {
+                $val = strval($val);
+                $first_zero = $val[0] === "0";
+
+                if (! $first_zero) {
+                        // 3. 30. 300. 3000. etc
+                        return 2;
+                }
+
+                $dot = false;
+                for ($i = 0; $i < strlen($val); $i++) {
+                        $c = $val[$i];
+                        if ($c === ".") {
+                                $dot = true;
+                                continue;
+                        }
+
+                        $is_nonzero = $c !== "0";
+                        if ($dot && $is_nonzero) {
+                                break;
+                        }
+                }
+                // 0. 0.0 0.00 0.000
+                return $i;
+        }
+
 	/** like ceil() only with the possibility to set significance
 	 * https://www.php.net/manual/en/function.ceil.php#85430
 	 **/
