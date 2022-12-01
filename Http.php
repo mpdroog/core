662eafc6994de79deec5326c33c06ff1eef5ba22
MP Droog <mpdroog@icloud.com>
2022-12-01 14:56:47 +0100

    Refactor. HTTP-class extended
    - always add header-data
    - feature to skip decoding
    - default to strict rules with API-usage

--- Http.php
+++ Http.php
@@ -7,16 +7,34 @@ class HTTP
 {
 	public static function json($url, array $opts)
 	{
+		$headers = [];
 		$ch = curl_init($url);
 		$ok = 1;
-		$ok &= curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
-		$ok &= curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		$ok &= curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-		$ok &= curl_setopt($ch, CURLOPT_TIMEOUT, 120);
-		$ok &= curl_setopt($ch, CURLOPT_HTTPHEADER, [
-		  "Accept: application/json"
-	  ]);
-		$ok &= curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+		$is_nodecode = isset($opts["NO_DECODE"]);
+                unset($opts["NO_DECODE"]);
+
+                // Strict defaults
+                $ok &= curl_setopt_array($ch, [
+                    CURLOPT_ENCODING => 'UTF-8',
+                    CURLOPT_FOLLOWLOCATION => 1,
+                    CURLOPT_AUTOREFERER => 1,
+                    CURLOPT_TIMEOUT => 120,
+                    CURLOPT_CONNECTTIMEOUT => 10,
+                    CURLOPT_HTTPHEADER => ["Accept: application/json"],
+                    CURLOPT_RETURNTRANSFER => 1,
+                    CURLOPT_SSL_VERIFYHOST => 2,
+                    CURLOPT_SSL_VERIFYPEER => 1,
+                    CURLOPT_CAINFO => ROOT . "/lib/cacert.pem", // Testing it from the xsnews-PHP view
+                    CURLOPT_SSLVERSION => 6,
+                ]);
+		$ok &= curl_setopt($ch, CURLOPT_HEADERFUNCTION, function($curl, $header) use (&$headers) {
+			$len = strlen($header);
+			$header = explode(':', $header, 2);
+			if (count($header) < 2) return $len; // ignore invalid
+			$headers[strtolower(trim($header[0]))][] = trim($header[1]);
+			return $len;
+		});
 		if ($ok !== 1) {
 			user_error("curl_setopt failed");
 		}
@@ -37,12 +55,11 @@ class HTTP
 			user_error('curl_exec fail');
 		}
 
-		$out = ["http" => $code, "ct" => $ct, "body" => $res];
-		if (Strings::has_prefix($ct, "application/json")) {
+		$out = ["http" => $code, "ct" => $ct, "head" => $headers, "body" => $res];
+		if (!$is_nodecode && Strings::has_prefix($ct, "application/json")) {
 			$out["body"] = json_decode($res, true);
 			if (! is_array($out["body"])) {
-				print_r($res);
-				user_error("http_json::failed decoding raw=%s", $res);
+				user_error(sprintf("http_json::failed decoding raw=%s", print_r($res, true)));
 			}
 		}
 		return $out;
